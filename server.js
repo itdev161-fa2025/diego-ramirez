d12f4c4dd6a5188d2f548a3b946fdcf94a7ad430
Your Name <you@example.com>
2024-09-15 23:09:52 -0500

    Completed Activity 3

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
+
+
+// Initialize express application
+const app = express();
+
+// Connect Database
+connectDatabase();
+
+// Configure Middleware
+app.use(express.json({extended: false}));
+
+// API endpoints
+/**
+ * @route GET /
+ * @desc Test Endpoint
+ */
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')
+);
+
+/**
+ * @route POST api/users
+ * @desc Registered user
+ */
+app.post("/api/users", 
+    [
+        check('name', 'Please enter your name')
+            .not()
+            .isEmpty(),
+        check('email', 'Please enter a valid email').
+            isEmail(),
+        check('password', 'Please enter a password with 6 or more characters')
+            .isLength({min: 6})
+    ],
+    (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+// Connection listener
+app.listen(3000, () => console.log('Express server running on port 3000'));
+
